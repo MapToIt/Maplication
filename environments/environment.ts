cba3dd7e19135727a457da81de773fdee7b55cd1
sfeeley <swildman@kent.edu>
2018-02-15 19:24:31 -0500

    Init repository

---
+++ environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDG4byzwgZFbsIdCE-BJGkP4HnZBn7PD1I",
+        authDomain: "maptoit-cece3.firebaseapp.com",
+        databaseURL: "https://maptoit-cece3.firebaseio.com",
+        projectId: "maptoit-cece3",
+        storageBucket: "maptoit-cece3.appspot.com",
+        messagingSenderId: "625166561557"
+    }
+  };
---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from './../../environments/environment';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { AppRoutingModule } from './/app-routing.module';
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AppNavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgbModule.forRoot(),
+    RouterModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

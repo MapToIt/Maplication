4849d30296caf5b3c3373cb4f002badf3f0bfe27
Patty Caldwell <pcaldwe4@kent.edu>
2018-02-16 19:39:24 -0500

    pc

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import {} from 'jasmine';
 
 describe('maplication App', () => {
   let page: AppPage;
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {} from 'jasmine';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({

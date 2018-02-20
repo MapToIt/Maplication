641c5ba7a67bbc027b9d408dd16f4d2317a5f84f
Christopher Elliott <35berge35@gmail.com>
2018-02-19 21:05:37 -0500

    Added attendee profile component

---
+++ src/app/attendee-profile/attendee-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attendee-profile',
+  templateUrl: './attendee-profile.component.html',
+  styleUrls: ['./attendee-profile.component.css']
+})
+export class AttendeeProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

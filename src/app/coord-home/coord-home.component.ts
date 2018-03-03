fb2fa246eb8d0058bb08e4527808fa6c34b34747
Patty Caldwell <pcaldwe4@kent.edu>
2018-03-03 09:37:15 -0500

    Table list

--- src/app/coord-home/coord-home.component.ts
+++ src/app/coord-home/coord-home.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coord-home.component.css']
 })
 export class CoordHomeComponent implements OnInit {
-
-  constructor() { }
-
+   public index = 0;
+  futures = [
+    {date: '05/01/2018', name: 'Kent Recruiters'},
+    {date: '05/30/2018', name: 'Portage County Recruitment'},
+    {date: '06/01/2018', name: 'IBM Recruitment'}
+  ]
+  pasts = [
+    {date: '10/01/2017', name: 'Cleveland Recruitment'},
+    {date: '10/30/2017', name: 'Engineering Recruitment'},
+    {date: '11/01/2017', name: 'Network Recruitment'},
+    {date: '12/01/2017', name: 'Tech Recruitment'},
+    {date: '01/25/2018', name: 'RN Recruitment'},
+    {date: '02/01/2018', name: 'Network Recruitment'}
+  ]
+  myPast = this.pasts[0];
+  myFuture = this.futures[0];
+  constructor() {    
+   }
+    public next(){
+    this.index = Math.min(this.index+5, this.pasts.length - 1);
+   }
   ngOnInit() {
+    
   }
 
 }

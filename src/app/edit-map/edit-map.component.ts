2ced33b11fcd103fb785c56298b6aa907bd023e6
Tyler <yanketyler@gmail.com>
2018-03-02 19:48:06 -0500

    Adds edit-map component.

---
+++ src/app/edit-map/edit-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-map',
+  templateUrl: './edit-map.component.html',
+  styleUrls: ['./edit-map.component.css']
+})
+export class EditMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
